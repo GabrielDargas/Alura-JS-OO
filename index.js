350328cba3fac0b1c99cb77b9feee9a574bbb219
GabrielDargas <68109894+GabrielDargas@users.noreply.github.com>
2020-07-19 12:17:34 -0300

    Aula 04 - 02

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const cliente1 = new cliente()
 cliente1.nome = "Ricardo"
 cliente1.cpf = 11122233309
 
-const cliente2 = new cliente()
-cliente2.nome = "Alice"
-cliente2.cpf = 88833366609
+//const cliente2 = new cliente()
+//cliente2.nome = "Alice"
+//cliente2.cpf = 88833366609
 
 const contaCorrenteRicardo = new contaCorrente()
 contaCorrenteRicardo.agencia = 1001
 contaCorrenteRicardo.cliente = cliente1
 
 const conta2 = new contaCorrente()
-conta2.cliente = cliente2
+conta2.cliente = new cliente()
+conta2.cliente.nome = "Alice"
+conta2.cliente.cpf = "88833366609"
 conta2.agencia = 1001
 
 contaCorrenteRicardo.depositar(500)
