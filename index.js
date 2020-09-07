e34ed529e49b95b823ab5bebe7aba4c77f5f293e
GabrielDargas <68109894+GabrielDargas@users.noreply.github.com>
2020-09-07 17:55:00 -0300

    Final Aula 01

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import { Cliente } from "./Cliente.js"
 import { contaCorrente } from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js"
+import { Conta } from "./Conta.js"
 
 const cliente1 = new Cliente("Ricardo", 11122233309)
 const cliente2 = new Cliente("Alice", 88833366609)
 
-const contaCorrenteRicardo = new contaCorrente(cliente1, 1001)
+const contaCorrenteRicardo = new Conta(0, cliente1, 1001)
 
 contaCorrenteRicardo.depositar(500)
 contaCorrenteRicardo.sacar(100)
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
+const contaPoupanca = new Conta(50, cliente1, 1001)
 
 console.log(contaPoupanca)
 console.log(contaCorrenteRicardo)
